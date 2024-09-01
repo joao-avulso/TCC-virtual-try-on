59de71a7d33210cce6d39b87cf0d7dd1dc4f6bab
joao-avulso <joaoalt0502@gmail.com>
2024-09-01 11:24:01 -0300

    Adicionado proporções de corpos

---
+++ ObjectControls.js
@@ -0,0 +1,46 @@
+import * as THREE from 'three';
+export class ObjectControls {
+    constructor(object, domElement) {
+        this.object = object;
+        this.domElement = domElement || document;
+
+        this.enabled = true;
+        this.rotationSpeed = 0.01;
+
+        this.state = 'none';
+
+        this.mouseStart = new THREE.Vector2();
+        this.mouseEnd = new THREE.Vector2();
+
+        this.domElement.addEventListener('mousedown', this.onMouseDown.bind(this), false);
+        this.domElement.addEventListener('mousemove', this.onMouseMove.bind(this), false);
+        this.domElement.addEventListener('mouseup', this.onMouseUp.bind(this), false);
+    }
+
+    onMouseDown(event) {
+        if (event.button === 0) { // Left mouse button
+            this.state = 'rotate';
+            this.mouseStart.set(event.clientX, event.clientY);
+        }
+    }
+
+    onMouseMove(event) {
+        if (!this.enabled) return;
+
+        this.mouseEnd.set(event.clientX, event.clientY);
+
+        if (this.state === 'rotate') {
+            this.rotate();
+        }
+
+        this.mouseStart.copy(this.mouseEnd);
+    }
+
+    onMouseUp() {
+        this.state = 'none';
+    }
+
+    rotate() {
+        this.object.rotateOnWorldAxis(new THREE.Vector3(0, 0, 1), (this.mouseEnd.x - this.mouseStart.x) * this.rotationSpeed);
+    }
+}
